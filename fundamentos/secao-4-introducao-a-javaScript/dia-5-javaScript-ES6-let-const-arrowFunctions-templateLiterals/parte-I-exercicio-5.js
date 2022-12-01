e9cee987412f6ed77baf54b317a8105df25fe89a
falecardoso <cardoso.fa@outlook.com>
2022-12-01 20:13:38 -0300

    fazendo arquivo separado para cada exercicio, parte-I-exercicio-5.js

---
+++ fundamentos/secao-4-introducao-a-javaScript/dia-5-javaScript-ES6-let-const-arrowFunctions-templateLiterals/parte-I-exercicio-5.js
@@ -0,0 +1,12 @@
+// function numeroAleatorio() {
+//     return Math.random()
+//   }
+//   console.log(numeroAleatorio());
+
+
+/**Passando para Arrow Functions: */
+
+  const numeroAleatorio = () => Math.random()
+    
+  
+  console.log(numeroAleatorio());
